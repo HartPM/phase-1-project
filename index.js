9eec092b69a68677e197b7e7fc3df2be7440c4df
Brody Losh <brodylosh@gmail.com>
2022-04-19 10:26:57 -0400

    Rendering Featured Image

--- index.js
+++ index.js
@@ -5,18 +5,23 @@ const veganURL = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=vegan'
 
 //Fetch
 fetch(vegetarianURL)
-.then(resp => resp.json())
-.then(recipes => (recipes.meals).forEach(recipe => unnamedFunction(recipe)
+    .then(resp => resp.json())
+    .then(recipes => (recipes.meals).forEach(recipe => unnamedFunction(recipe)
 ))
 
 fetch(veganURL)
-.then(resp => resp.json())
-.then(recipes => (recipes.meals).forEach(recipe => otherFunction(recipe)
+    .then(resp => resp.json())
+    .then(recipes => (recipes.meals).forEach(recipe => otherFunction(recipe)
 ))
 
 
 //DOM Selecters
 let dropdownMenu = document.querySelector('#form')
+let recipesUl = document.querySelector('#recipes')
+let featureName = document.querySelector('#feature-name')
+let recipeImage = document.querySelector('#recipe-image')
+let recipeName = document.querySelector('#recipe-title')
+let recipeBody = document.querySelector('#recipe-body')
 
 
 //Event Listeners
@@ -24,16 +29,41 @@ dropdownMenu.addEventListener('change', filterData)
 
 
 //Functions
-function unnamedFunction(object) {
-    console.log(object)
+function unnamedFunction(recipe) {
+    featuredRecipe(recipe)
 }
 
-function otherFunction(object) {
-    console.log(object)
+function otherFunction(recipe) {
+    //console.log(recipe)
 }
+
+function addRecipe(recipe) {
+    let newLi = document.createElement('li')
+    newLi.textContent = 
+    recipesUl.appendChild(newLi)
+    newLi.addEventListener('click', displayRecipe)
+}
+
+function featuredRecipe(recipe) {
+    recipeImage.src = recipe.strMealThumb
+    featureName.textContent = recipe.strMeal
+    recipeName.textContent = recipe.strMeal
+    fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + `${recipe.idMeal}`)
+    .then(resp => resp.json())
+    .then(data => {let instructions = data.meals[0].strInstructions
+        console.log(instructions)
+    })
+
+    
+}
+
+function renderBody(instructions) {
+
+}
+
 
 
 //Event Listener Callback Functions
 function filterData(e) {
-    console.log(e.target)
+    //console.log(e.target)
 }
